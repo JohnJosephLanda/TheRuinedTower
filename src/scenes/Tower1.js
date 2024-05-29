13dd8bd0778338767a1fe2085b2dd710a2606965
JohnJosephLanda <jjl2008.com@gmail.com>
2024-05-29 13:24:53 -0400

    Started second level

---
+++ src/scenes/Tower1.js
@@ -0,0 +1,57 @@
+import Phaser from "phaser"
+
+import Player from "../sprites/Player"
+
+export default class Tower1 extends Phaser.Scene
+{
+    // player;
+    preload()
+    {
+        this.load.image('player','src/images/player.png')
+    }
+
+    create()
+    {
+        // working on adding the player
+
+        // player = new Player({
+        //     scene: this,
+        //     x: 400,
+        //     y: 400
+        // })
+        // this.physics.add.existing(player)
+        // player.body.setCollideWorldBounds(true, 0, 0)
+
+        this.ball = this.add.circle(600, 300, 7, 0xffffff)
+        this.physics.add.existing(this.ball)
+        this.ball.body.setCollideWorldBounds(true, 0, 0)
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // player.normalPhysics(this.cursors)
+        
+        // testing for the ball
+        let character = this.ball.body
+        
+        if (this.cursors.up.isDown && (character.onFloor())) {
+            character.setVelocityY(-100)
+        }
+        if (this.cursors.right.isDown) {
+            if (character.velocity.x < 25) character.setVelocityX(character.velocity.x+2)
+                character.setAccelerationX(50)
+        }
+        else if (this.cursors.left.isDown) {
+            if (character.velocity.x > -25) character.setVelocityX(character.velocity.x-2)
+            character.setAccelerationX(-50)
+        }
+        else {
+            character.setAccelerationX(-character.velocity.x*4)
+        }
+
+        if (character.x < 5) {
+            this.scene.pause()
+        }
+    }
+}
