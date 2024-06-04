65bc82ab1a33dfd673be5fbddf247f258c781bf2
JohnJosephLanda <jjl2008.com@gmail.com>
2024-06-04 13:23:36 -0400

    Added second/fourth level

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Phaser from 'phaser'
 import TitleScreen from './scenes/TitleScreen'
 import Start from './scenes/Start'
 import Tower1 from './scenes/Tower1'
+import Tower2 from './scenes/Tower2'
 
 const config = {
     width: 800,
@@ -24,11 +25,12 @@ const game = new Phaser.Game(config)
 game.scene.add('titlescreen',TitleScreen)
 game.scene.add("startingPoint",Start)
 game.scene.add("tower1",Tower1)
+game.scene.add("tower2",Tower2)
 
 // this.load.image('player','src/images/player.png')
 // const playerTexture = new Phaser.Textures.Texture(new Phaser.Textures.TextureManager('game'),'playerIMG','player')
 // const player = new Phaser.GameObjects.Sprite('game',400,500)
 
 // game.scene.start('titlescreen')
-game.scene.start("startingPoint");
-// game.scene.start("tower1"); // temp to work on level
+// game.scene.start("startingPoint");
+game.scene.start("tower2"); // temp to work on level
