a78e46544df3bb4b29c52a492e0b5c4c2eafb866
JohnJosephLanda <jjl2008.com@gmail.com>
2024-06-04 22:07:23 -0400

    small main fix

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import TitleScreen from './scenes/TitleScreen'
 import Start from './scenes/Start'
 import Tower1 from './scenes/Tower1'
 import Tower2_4 from './scenes/Tower2_4'
+import Tower3 from './scenes/Tower3'
+import Tower2_4from3 from './scenes/Tower2_4from3'
 
 const config = {
     width: 800,
@@ -27,11 +29,12 @@ game.scene.add('titlescreen',TitleScreen)
 game.scene.add("startingPoint",Start)
 game.scene.add("tower1",Tower1)
 game.scene.add("tower2_4",Tower2_4)
+game.scene.add("tower3",Tower3)
+game.scene.add("tower2_4from3",Tower2_4from3)
 
 // this.load.image('player','src/images/player.png')
 // const playerTexture = new Phaser.Textures.Texture(new Phaser.Textures.TextureManager('game'),'playerIMG','player')
 // const player = new Phaser.GameObjects.Sprite('game',400,500)
 
-// game.scene.start('titlescreen')
 game.scene.start("titlescreen");
-// game.scene.start("tower2_4"); // temp to work on level
+// game.scene.start("tower3"); // temp to work on level
