a6a647243868c1c4a690abbf57f03f5e2b7619bf
JohnJosephLanda <jjl2008.com@gmail.com>
2024-06-13 10:05:16 -0400

    ran code

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38481" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38683" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
